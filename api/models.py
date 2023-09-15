ac2435b97cc92372a2f80c577eeff1366ee58237
luciocatilina <pablocandia395@gmail.com>
2023-09-14 23:12:47 -0300

    hola

--- api/models.py
+++ api/models.py
@@ -4,7 +4,8 @@ from django.db import models
 
 class Project(models.Model):
     name = models.CharField(max_length=25, blank=False, null=False)
-    description = models.TextField(blank=False, null=False)
+    image = models.ImageField(upload_to= 'images', null=True, blank=True)
+    description = models.TextField(blank=True, null=True)
     tools = models.TextField(blank=False, null=False)
     repository = models.TextField(blank=False, null=False)
     host = models.TextField(blank=True, null=True)
--- api/viewsets.py
+++ api/viewsets.py
@@ -1,13 +1,42 @@
 from rest_framework import viewsets
 from .models import SmallProject, BigProject
 from .serializer import SmallProjectSerializer, BigProjectSerializer
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated, BasePermission
+
+
+
+class ApiAccessPermission(BasePermission):
+    """
+    Custom permission class to allow unauthenticated access for API calls.
+    """
+
+    def has_permission(self, request, view):
+        # Verifica si la solicitud proviene de una fuente confiable, como tu aplicación web
+        if request.META.get('HTTP_REFERER') and 'tusitio.com' in request.META.get('HTTP_REFERER'):
+            return True
+        # Si no proviene de tu aplicación web, requiere autenticación
+        return request.user and request.user.is_authenticated
 
 class SmallProjectViewSet(viewsets.ModelViewSet):
+    queryset = SmallProject.objects.all()
+    serializer_class = SmallProjectSerializer
 
-    queryset=SmallProject.objects.all()
-    serializer_class=SmallProjectSerializer
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:
+            return [ApiAccessPermission()]
+        return []
 
 class BigProjectViewSet(viewsets.ModelViewSet):
+    queryset = BigProject.objects.all()
+    serializer_class = BigProjectSerializer
 
-    queryset=BigProject.objects.all()
-    serializer_class=BigProjectSerializer
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:
+            return [ApiAccessPermission()]
+        return []
+
